27da78d93c2516331ed1d95330639166b5a634bc
vidiavrd <andhikasmuchammad@gmail.com>
2020-01-26 16:13:51 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -179,6 +179,7 @@ $('document').ready(function(){
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
+					$("p:nth-child(49)").fadeIn('slow').delay(1000).animate({top:500},500);
 				});
 				
 			}
